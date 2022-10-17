3c84ed4fa428c9ea4d1665d7abe29e3465baf8fe
vishal <vishaltaral09@gamil.com>
2022-10-17 16:34:55 +0530

    prototype file added

---
+++ prototypal_inheritance/prototype.js
@@ -0,0 +1,11 @@
+let student = {
+    name:"vishal",
+    city: "Sambhaji nagar",
+};
+let teacher = {
+    noun:"Milind",
+    district:"jalgaon",
+
+};
+teacher.__proto__=student;
+ console.log(teacher);
