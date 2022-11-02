d67ce12ef7f190b6ceedba6987040c166df500cb
vishal <vishaltaral09@gamil.com>
2022-11-02 12:40:05 +0530

    dom named folder added

---
+++ DOM/dom.js
@@ -0,0 +1,7 @@
+let xyz =document.getElementById('one');
+console.log(xyz);
+xyz.style.color="blue"
+
+console.log( document.body.constructor.name );
+console.log( document.body instanceof HTMLBodyElement );
+console.log(elem);
